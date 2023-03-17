e2d9c0e8771ed1003a036cf1c2e862864918d89a
Ary <demitropulosary@gmail.com>
2023-03-17 16:56:14 -0300

    Se configura JEST para poder hacer pruebas sobre el thunk

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,5 @@
 module.exports = {
-    testEnvironment: 'jest-environment-jsdom',
-    setupFiles: ['./jest.setup.js']
-}
+  testEnvironment: 'jest-environment-jsdom',
+  setupFiles: ['./jest.setup.js'],
+  transformIgnorePatterns: [], //Para que los paquetes de node no sean ignorados //Para que no haga transpilaciones de las cosas de firebase
+};
